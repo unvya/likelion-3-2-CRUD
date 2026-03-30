4c1e9d26d1bedd0b78ba719c864b4a33ed654739
unvya <dnskoe7@gmail.com>
2026-03-31 02:01:56 +0900

    CRUD 적용

--- myblogProject/experiences/urls.py
+++ myblogProject/experiences/urls.py
@@ -5,4 +5,7 @@ from experiences import views
 urlpatterns = [
     path("", views.experience, name="experiences"),
     path("create/", views.create, name="create"),
+    path("<int:post_id>/", views.experience_detail, name="experience_detail"),
+    path("experience_update/<int:id>/", views.experience_update, name="experience_update"),
+    path("experience_delete/<int:id>/", views.experience_delete, name="experience_delete"),
 ]
--- myblogProject/experiences/views.py
+++ myblogProject/experiences/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostModelForm
+from experiences.models import Post
 
 # Create your views here.
 def experience(request):
-    return render(request, "experience.html")
+    experiences = Post.objects.all().order_by('-created_at')
+    return render(request, "experience.html", {"experiences" : experiences})
 
 def create(request):
     if request.method == 'POST':
@@ -14,3 +16,24 @@ def create(request):
     else:
         form = PostModelForm()
     return render(request, 'form_create.html', {'form': form})
+
+def experience_detail(request, post_id):
+    experience = get_object_or_404(Post, pk=post_id)
+    return render(request, "experience_detail.html", {"experience" : experience})
+
+def experience_update(request, id):
+    experience = get_object_or_404(Post, pk=id)
+
+    if request.method == 'POST':
+        form = PostModelForm(request.POST, instance=experience)
+        if form.is_valid():
+            form.save()
+            return redirect('experiences')
+    else:   #GET이면
+        form = PostModelForm(instance=experience)
+        return render(request, 'form_create.html', {'form':form, 'id':id})
+    
+def experience_delete(request, id):
+    experience = Post.objects.get(pk=id)
+    experience.delete()
+    return redirect('experiences')
